1a7d81546641e76f5e1a184de6d8d70cc2ddbf4b
kannachi323 <mattdev727@gmail.com>
2026-04-22 18:34:17 -0700

    skip misty bin test

--- client/cmake/setup_tests.cmake
+++ client/cmake/setup_tests.cmake
@@ -1,12 +1,13 @@
 # cmake/setup_tests.cmake
 
-file(GLOB_RECURSE TEST_SRCS
-    "src/tests/*.cpp"
-    "src/tests/*.h"
-)
-# tests
-add_executable(misty_tests ${TEST_SRCS})
-target_link_libraries(misty_tests PRIVATE
-    misty_core
-    gtest_main
-)
+if(CMAKE_BUILD_TYPE STREQUAL "Debug" OR CMAKE_BUILD_TYPE STREQUAL "RelWithDebInfo")
+    file(GLOB_RECURSE TEST_SRCS
+        "src/tests/*.cpp"
+        "src/tests/*.h"
+    )
+    add_executable(misty_tests ${TEST_SRCS})
+    target_link_libraries(misty_tests PRIVATE
+        misty_core
+        gtest_main
+    )
+endif()
